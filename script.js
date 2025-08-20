f8139502ca171b7b61931251ff6e181ea01e80a1
Qurbonbekov Sherzod <qurbonbekovsher2004@gmail.com>
2025-08-20 16:17:32 +0500

    myhomework

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 
+let book = "Game of thrones";
+const Author = "George Martin";
+const year = 1996;
+const isRead = true;
+// comments
+
+console.log("Book:" + book);
+console.log("Author:" + Author);
+console.log("Year:" + year);
+console.log("Is read:" + (isRead));
+// inspect 
+
+console.log("Type book:", typeof book);
+console.log("Type Author:", typeof Author);
+console.log("Type isRead:",typeof isRead)
+console.log("Type year:",typeof year);
